9166927cbb4a32df0d51de6e8992b84c58d13ebb
takus <ec2-user@ip-172-31-34-180.ap-northeast-1.compute.internal>
2020-07-21 00:04:16 +0900

    layout add

--- form/data.php
+++ form/data.php
@@ -1,14 +1,30 @@
 <?php
 
-    class Form{
+    class Form
+    {
         private $title = "お問合わせはこちらから";
-        private $name;
-        private $age;
-        private $sex;
+        private $name = "名前";
+        private $age = "年齢";
+        private $sex = array("男", "女");
         private $textArea;
         
-        public function getTitle(){
+        public function getTitle()
+        {
             return $this->title;
         }
+        
+        public function getName()
+        {
+            return $this->name;
+        }
+        
+        public function getSex($sex)
+        {
+            return $this->sex[$sex];
+        }
+        
+        public function getAge()
+        {
+            return $this->age;
+        }
     }
-?>
